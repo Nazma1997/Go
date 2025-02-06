1ba5d5e260474c009cf48ae52686fbffe2d47142
nazma1997 <nazma.ist.707@gmail.com>
2025-02-06 23:36:00 +0600

    3 types array declation

---
+++ day-2/array.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+func main(){
+
+	// Array declaration  type 1
+	arrayType1 := [...]int{1,2,3,4}
+	fmt.Println(arrayType1)
+
+	// Array declaration  type 2
+	arrayType2 := [...]int{1,2,3,4}
+	fmt.Println( arrayType2)
+
+	// Array declaration  type 3
+	arrayType3 := [4]int{1,2,3,4}
+	fmt.Println( arrayType3)
+	
+}
+
